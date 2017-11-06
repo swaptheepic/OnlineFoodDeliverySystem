d5c547b331ece79f11812e304e8cc098b6ea3bdc
ezkarpr <ezkarpr@PGGW023.ericsson.se>
2017-11-06 14:08:00 +0530

    P

--- OnlineFoodDeliverySystem/src/Ashwini.java
+++ OnlineFoodDeliverySystem/src/Ashwini.java
@@ -3,7 +3,7 @@ public class Ashwini {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HELLO ASHWINI!!");
+		System.out.println("HELLO ASHWINI!! from P");
 
 	}
 }
