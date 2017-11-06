7846d33b7f1a186428dca17bca75b39e8c93f75e
ezbabsw <ezbabsw@5CG5011TNB.ericsson.se>
2017-11-06 14:10:40 +0530

    ALL CLEAR NOW!

--- OnlineFoodDeliverySystem/src/Ashwini.java
+++
@@ -1,9 +0,0 @@
-
-public class Ashwini {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HELLO ASHWINI!! from P");
-
-	}
-}
--- OnlineFoodDeliverySystem/src/prats11.java
+++
@@ -1,7 +0,0 @@
-
-public class prats11 {
-	public static void main(String args[])
-	{
-		System.out.println("Hello Pratiksha, here! ");
-	}
-}
