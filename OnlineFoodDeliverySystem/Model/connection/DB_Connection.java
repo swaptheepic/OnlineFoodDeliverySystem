570c7fa09581260ad926b6c6e96a5fdafd15c0f8
ezbabsw <ezbabsw@5CG5011TNB.ericsson.se>
2017-11-06 15:02:14 +0530

    DB UPDATE AND CONNECTOR

--- OnlineFoodDeliverySystem/Model/connection/DB_Connection.java
+++ OnlineFoodDeliverySystem/Model/connection/DB_Connection.java
@@ -5,27 +5,24 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 
 public class DB_Connection {
-	
-	
+
 	public static void main(String[] args) {
 		DB_Connection obj_DB_Connection = new DB_Connection();
 		System.out.println(obj_DB_Connection.get_connection());
 	}
-	
-	
+
 	public Connection get_connection() {
-		
+
 		Connection connection = null;
-		
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-	        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/task", "root", "root");
-		}
-		catch(Exception e){
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ofd_db", "root", "root");
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return connection;
-		
+
 	}
 
 }
